354a55f7440b70ac564d6cceed1f44b0ce003e6f
Teal <teal.dillon@gmail.com>
2023-11-02 12:14:36 -0700

    Linting and versioning.

--- astrodata/version.py
+++ astrodata/version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev2+ga7e7c38.d20231017"
-__version_tuple__ = version_tuple = (0, 1, "dev2", "ga7e7c38.d20231017")
+__version__ = version = "0.1.dev37+g417875b.d20231030"
+__version_tuple__ = version_tuple = (0, 1, "dev37", "g417875b.d20231030")
