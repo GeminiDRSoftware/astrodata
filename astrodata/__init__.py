281258431a485e0901480f191fb1361a037e3dd1
Teal <teal.dillon@gmail.com>
2023-11-02 11:53:38 -0700

    Update docstring, __all__, and deprecate open
    Adds/fixes some replacements in the module docstring. Organize the
    module contents.
    
    Biggest change here is that open is now flagged as deprecated. There
    should not be any overrides to python builtins in the astrodata release.
    It also can be confused with astropy.io.fits.open (also overriding a
    builtin, so there are two points of possible conflict).
    
    Open to name changes, but a firm on removing all builtin overrides.

--- astrodata/__init__.py
+++ astrodata/__init__.py
@@ -2,35 +2,50 @@
 information contained in the headers as attributes. To do so, one must subclass
 :class:`astrodata.AstroData` and add parse methods accordingly to the
 :class:`~astrodata.TagSet` received.
+
+.. |AstroData| replace:: :class:`~astrodata.AstroData`
+.. |AstroDataError| replace:: :class:`~astrodata.AstroDataError`
+.. |AstroDataMixin| replace:: :class:`~astrodata.AstroDataMixin`
+.. |NDAstroData| replace:: :class:`~astrodata.NDAstroData`
+.. |Section| replace:: :class:`~astrodata.Section`
+.. |TagSet| replace:: :class:`~astrodata.TagSet`
+.. |astro_data_descriptor| replace:: :func:`~astrodata.astro_data_descriptor`
+.. |astro_data_tag| replace:: :func:`~astrodata.astro_data_tag`
+.. |create| replace:: :func:`~astrodata.create`
+.. |open| replace:: :func:`~astrodata.open`
+.. |return_list| replace:: :func:`~astrodata.return_list`
+.. |version| replace:: :func:`~astrodata.version`
 """
 
 __all__ = [
     "AstroData",
     "AstroDataError",
-    "TagSet",
-    "NDAstroData",
     "AstroDataMixin",
+    "NDAstroData",
+    "Section",
+    "TagSet",
+    "__version__",
+    "add_header_to_table",
     "astro_data_descriptor",
     "astro_data_tag",
-    "open",
     "create",
-    "__version__",
+    "open",
+    "returns_list",
     "version",
-    "add_header_to_table",
-    "Section",
 ]
 
 
 from .core import AstroData
-from .fits import add_header_to_table
 from .factory import AstroDataFactory, AstroDataError
+from .fits import add_header_to_table
 from .nddata import NDAstroData, AstroDataMixin
 from .utils import (
+    Section,
     TagSet,
     astro_data_descriptor,
     astro_data_tag,
-    Section,
     returns_list,
+    deprecated,
 )
 from ._version import version
 
@@ -41,6 +56,20 @@ factory = AstroDataFactory()
 # (if we're dealing with a FITS file)
 factory.add_class(AstroData)
 
-# TODO: Need to replace this with a name that doesn't override the builtin
-open = factory.get_astro_data
+
+# TODO: Need to replace this with a name that doesn't override the builtin.
+# This makes it so that the following will cause unexpected behavior:
+#     from astrodata import *
+#     file_stream = open("some_file.fits")
+# Without raising a warning or error.
+@deprecated(
+    "Use 'astrodata.from_file'. astrodata.open is deprecated, "
+    "and will be removed in a future version."
+)
+def open(*args, **kwargs):  # pylint: disable=redefined-builtin
+    """Return an |AstroData| object from a file."""
+    return factory.get_astro_data(*args, **kwargs)
+
+
+from_file = factory.get_astro_data
 create = factory.create_from_scratch
