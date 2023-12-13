f4400ef7cbfb87a92ff51295ca94a07c0523adb2
Teal <teal.dillon@gmail.com>
2023-12-13 14:17:19 -0800

    Update open->from_file, add TODO about factory
    astrodata.factory is overriden by a variable assignment in __init__.py,
    which in turn means that astrodata imports cannot reach any of the
    internals for the factory classes.
    
    While I can understand the immediate rationale here, this has to change.
    I suspect the best way forward here is to rename the factory.py file to
    something else, since the variable reassignment here is assumed
    throughout many, many tests and such.

--- astrodata/__init__.py
+++ astrodata/__init__.py
@@ -3,39 +3,7 @@ information contained in the headers as attributes. To do so, one must subclass
 :class:`astrodata.AstroData` and add parse methods accordingly to the
 :class:`~astrodata.TagSet` received.
 
-.. |AstroData| replace:: :class:`~astrodata.AstroData`
-.. |AstroDataError| replace:: :class:`~astrodata.AstroDataError`
-.. |AstroDataMixin| replace:: :class:`~astrodata.AstroDataMixin`
-.. |NDAstroData| replace:: :class:`~astrodata.NDAstroData`
-.. |Section| replace:: :class:`~astrodata.Section`
-.. |TagSet| replace:: :class:`~astrodata.TagSet`
-.. |astro_data_descriptor| replace:: :func:`~astrodata.astro_data_descriptor`
-.. |astro_data_tag| replace:: :func:`~astrodata.astro_data_tag`
-.. |create| replace:: :func:`~astrodata.create`
-.. |open| replace:: :func:`~astrodata.open`
-.. |return_list| replace:: :func:`~astrodata.return_list`
-.. |version| replace:: :func:`~astrodata.version`
 """
-
-__all__ = [
-    "AstroData",
-    "AstroDataError",
-    "AstroDataMixin",
-    "NDAstroData",
-    "Section",
-    "TagSet",
-    "__version__",
-    "add_header_to_table",
-    "astro_data_descriptor",
-    "astro_data_tag",
-    "from_file",
-    "create",
-    "open",
-    "returns_list",
-    "version",
-]
-
-
 from .core import AstroData
 from .factory import AstroDataFactory, AstroDataError
 from .fits import add_header_to_table
@@ -50,12 +18,58 @@ from .utils import (
 )
 from ._version import version
 
+__all__ = [
+    "AstroData",
+    "AstroDataError",
+    "AstroDataMixin",
+    "NDAstroData",
+    "Section",
+    "TagSet",
+    "__version__",
+    "add_header_to_table",
+    "astro_data_descriptor",
+    "astro_data_tag",
+    "from_file",
+    "create",
+    "returns_list",
+    "version",
+    # Below this are deprecated
+    "open",
+]
+
+# Make sure __all__does not have duplicates
+if len(__all__) != len(set(__all__)):
+    duplicates = [x for i, x in enumerate(__all__) if x in __all__[:i]]
+    raise ValueError(f"Duplicate entries in __all__: {', '.join(duplicates)}")
+
 __version__ = version()
 
+# TODO: This is pretty unacceptable, this makes it impossible to access the
+# factory classes, errors, and methods through any normal means. Which means
+# they are very difficult to write tests for, or to have users work on
+# themselves.
 factory = AstroDataFactory()
 # Let's make sure that there's at least one class that matches the data
 # (if we're dealing with a FITS file)
 factory.add_class(AstroData)
+
+
+def from_file(*args, **kwargs):
+    """Return an |AstroData| object from a file.
+
+    For implementation details, see
+    :meth:`~astrodata.AstroDataFactory.get_astro_data`.
+    """
+    return factory.get_astro_data(*args, **kwargs)
+
+
+def create(*args, **kwargs):
+    """Return an |AstroData| object from data.
+
+    For implementation details, see
+    :meth:`~astrodata.AstroDataFactory.create_from_scratch`
+    """
+    return factory.create_from_scratch(*args, **kwargs)
 
 
 # TODO: Need to replace this with a name that doesn't override the builtin.
@@ -68,9 +82,7 @@ factory.add_class(AstroData)
     "and will be removed in a future version."
 )
 def open(*args, **kwargs):  # pylint: disable=redefined-builtin
-    """Return an |AstroData| object from a file."""
-    return factory.get_astro_data(*args, **kwargs)
-
-
-from_file = factory.get_astro_data
-create = factory.create_from_scratch
+    """Return an |AstroData| object from a file (deprecated, use
+    :func:`~astrodata.from_file`).
+    """
+    return from_file(*args, **kwargs)
