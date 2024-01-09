3b903131c7d14e6654319f58ca7ae30fb75552f0
Teal <teal.dillon@gmail.com>
2024-01-09 11:28:08 -0800

    Add deprecation testing.

---
+++ astrodata/tests/test_deprecated.py
@@ -0,0 +1,74 @@
+"""This test file is meant to test that deprecated functions either raise the
+appropriate warning or error. It does not check for their functionality (which
+is covered by the functions that are replacing them, anyways).
+"""
+import astrodata
+import astropy.io.fits as fits
+import pytest
+
+from astrodata.utils import AstroDataDeprecationWarning
+
+
+@pytest.fixture
+def example_fits_file(tmpdir) -> str:
+    """Create an empty fits file, and return the path."""
+    filename = tmpdir.join("example.fits")
+    hdu = fits.PrimaryHDU()
+    hdu.writeto(filename)
+    return filename
+
+
+@pytest.fixture
+def example_ad_class():
+    class ExampleAdClass(astrodata.AstroData):
+        pass
+
+    return ExampleAdClass
+
+
+@pytest.fixture
+def example_phu():
+    return fits.PrimaryHDU(data=[1, 2, 3])
+
+
+@pytest.fixture
+def example_extensions(example_phu):
+    extension = fits.ImageHDU(data=[1, 2, 3])
+    return [example_phu, extension]
+
+
+@pytest.fixture
+def ad(example_fits_file):
+    return astrodata.from_file(example_fits_file)
+
+
+def test_open(example_fits_file):
+    with pytest.warns(AstroDataDeprecationWarning):
+        astrodata.open(example_fits_file)
+
+
+def test_deprecated_astrodata_header(ad):
+    with pytest.warns(AstroDataDeprecationWarning):
+        ad.header
+
+
+def test_deprecated_astrodatafactory_openFile(example_fits_file):
+    with pytest.warns(AstroDataDeprecationWarning):
+        astrodata.factory._openFile(example_fits_file)
+
+
+def test_deprecated_astrodatafactory_addClass(example_ad_class):
+    with pytest.warns(AstroDataDeprecationWarning):
+        astrodata.factory.addClass(example_ad_class)
+
+
+def test_deprecated_astrodatafactory_getAstroData(example_fits_file):
+    with pytest.warns(AstroDataDeprecationWarning):
+        astrodata.factory.getAstroData(example_fits_file)
+
+
+def test_deprecated_astrodatafactory_createFromScratch(
+    example_phu, example_extensions
+):
+    with pytest.warns(AstroDataDeprecationWarning):
+        astrodata.factory.createFromScratch(example_phu, example_extensions)
