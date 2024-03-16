a1bbc626ae014a1f292b28ed4856bbe8c2fcd3a6
Teal <teal.dillon@gmail.com>
2024-03-15 18:04:20 -0700

    Update pyproject.toml with 'all' deps

--- astrodata/version.py
+++ astrodata/version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev196+g7789fd2.d20240315"
-__version_tuple__ = version_tuple = (0, 1, "dev196", "g7789fd2.d20240315")
+__version__ = version = "0.1.dev203+g00fb6aa.d20240316"
+__version_tuple__ = version_tuple = (0, 1, "dev203", "g00fb6aa.d20240316")
