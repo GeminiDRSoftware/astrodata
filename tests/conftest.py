d67a1fce84dc7e7bc4192fd0cfb24fcb4c075dca
Teal <teal.dillon@gmail.com>
2024-04-01 11:31:32 -0700

    Add tests for REFCAT, test cache fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Configuration for astrodata tests (using pytest)."""
 
 import importlib
+import os
 
 import pytest
 
@@ -10,3 +11,26 @@ import pytest
 def reload_astrodata():
     """Reload the astrodata module."""
     importlib.reload(importlib.import_module("astrodata"))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def assign_test_cache(tmp_path_factory):
+    """Assign a temporary cache directory for testing."""
+    # Set ASTRODATA_TEST to the temporary directory if it is not already set.
+    result = None
+    tmpdir = tmp_path_factory.mktemp(".testing_cache")
+
+    if not (result := os.environ.get("ASTRODATA_TEST")):
+        os.environ["ASTRODATA_TEST"] = os.path.join(
+            tmpdir, "archive_downloads"
+        )
+
+    yield
+
+    # Unset the ASTRODATA_TEST environment variable (in case python session
+    # lasts beyond pytest lifetime for some reason).
+    if not result:
+        os.environ.pop("ASTRODATA_TEST")
+
+    else:
+        os.environ["ASTRODATA_TEST"] = result
